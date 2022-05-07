a06c7a8fd0b115fa69c0557f8d1b48ccfa6d75d9
Yury Uvarov <uvarov.frontend@gmail.com>
2022-05-08 00:23:22 +0300

    add documentation

--- dest/js/main.js
+++ dest/js/main.js
@@ -21,7 +21,93 @@ document.addEventListener('DOMContentLoaded', function () {
   three.name.months.kz = ['Қаңтар', 'Ақпан', 'Наурыз', 'Сәуір', 'Мамыр', 'Маусым', 'Шілде', 'Тамыз', 'Қыркүйек', 'Қазан', 'Қараша', 'Желтоқсан'];
   three.name.week.kz = ['Си', 'Же', 'Ду', 'Сй', 'Ср', 'Бе', 'Жұ'];
   three.settings.lang = 'kz';
-  three.init(); // Temp
+  three.init();
+  var four = new VanillaCalendar({
+    calendar: document.querySelector('#four'),
+    settings: {
+      selecting: false
+    }
+  });
+  four.init();
+  var five = new VanillaCalendar({
+    calendar: document.querySelector('#five'),
+    settings: {
+      weekend: false,
+      today: false
+    }
+  });
+  five.init();
+  var six = new VanillaCalendar({
+    calendar: document.querySelector('#six'),
+    today: new Date('2022-01-07'),
+    settings: {
+      selected: {
+        date: '2022-01-09',
+        month: 1,
+        year: 2022
+      }
+    }
+  });
+  six.init();
+  var seven = new VanillaCalendar({
+    calendar: document.querySelector('#seven'),
+    today: new Date('2022-01-08'),
+    settings: {
+      selected: {
+        month: 1,
+        year: 2022
+      },
+      range: {
+        min: '2022-01-07',
+        max: '2022-02-12',
+        values: ['2022-01-02', '2022-01-04']
+      }
+    }
+  });
+  seven.init();
+  var eight = new VanillaCalendar({
+    calendar: document.querySelector('#eight'),
+    today: new Date('2022-01-08'),
+    settings: {
+      selected: {
+        month: 1,
+        year: 2022,
+        holidays: ['2022-01-03', '2022-01-04', '2022-01-05', '2022-01-20']
+      }
+    }
+  });
+  eight.init();
+  var nine = new VanillaCalendar({
+    calendar: document.querySelector('#nine'),
+    today: new Date('2022-02-08'),
+    settings: {
+      selected: {
+        month: 2,
+        year: 2022
+      },
+      range: {
+        min: '2022-01-07',
+        max: '2022-02-12'
+      },
+      visibility: {
+        months: false
+      }
+    }
+  });
+  nine.init();
+  var ten = new VanillaCalendar({
+    calendar: document.querySelector('#ten'),
+    settings: {
+      visibility: {
+        year: false,
+        arrows: {
+          prev: false,
+          next: false
+        }
+      }
+    }
+  });
+  ten.init(); // Temp
 
   hljs.highlightAll();
 });
--- src/js/main.js
+++ src/js/main.js
@@ -23,6 +23,99 @@ document.addEventListener('DOMContentLoaded', () => {
 	three.settings.lang = 'kz';
 	three.init();
 
+	const four = new VanillaCalendar({
+		calendar: document.querySelector('#four'),
+		settings: {
+			selecting: false,
+		},
+	});
+	four.init();
+
+	const five = new VanillaCalendar({
+		calendar: document.querySelector('#five'),
+		settings: {
+			weekend: false,
+			today: false,
+		},
+	});
+	five.init();
+
+	const six = new VanillaCalendar({
+		calendar: document.querySelector('#six'),
+		today: new Date('2022-01-07'),
+		settings: {
+			selected: {
+				date: '2022-01-09',
+				month: 1,
+				year: 2022,
+			},
+		},
+	});
+	six.init();
+
+	const seven = new VanillaCalendar({
+		calendar: document.querySelector('#seven'),
+		today: new Date('2022-01-08'),
+		settings: {
+			selected: {
+				month: 1,
+				year: 2022,
+			},
+			range: {
+				min: '2022-01-07',
+				max: '2022-02-12',
+				values: ['2022-01-02', '2022-01-04'],
+			},
+		},
+	});
+	seven.init();
+
+	const eight = new VanillaCalendar({
+		calendar: document.querySelector('#eight'),
+		today: new Date('2022-01-08'),
+		settings: {
+			selected: {
+				month: 1,
+				year: 2022,
+				holidays: ['2022-01-03', '2022-01-04', '2022-01-05', '2022-01-20'],
+			},
+		},
+	});
+	eight.init();
+
+	const nine = new VanillaCalendar({
+		calendar: document.querySelector('#nine'),
+		today: new Date('2022-02-08'),
+		settings: {
+			selected: {
+				month: 2,
+				year: 2022,
+			},
+			range: {
+				min: '2022-01-07',
+				max: '2022-02-12',
+			},
+			visibility: {
+				months: false,
+			},
+		},
+	});
+	nine.init();
+
+	const ten = new VanillaCalendar({
+		calendar: document.querySelector('#ten'),
+		settings: {
+			visibility: {
+				year: false,
+				arrows: {
+					prev: false,
+					next: false,
+				},
+			},
+		},
+	});
+	ten.init();
+
 	// Temp
 	hljs.highlightAll();
 });
